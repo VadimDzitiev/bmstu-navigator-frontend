ae1ec590b77b8b91d12f3f1bb88306764febb0b3
VadimDzitiev <vdzitiev@inbox.ru>
2024-01-26 09:03:49 +0300

    1

--- src/consts.ts
+++ src/consts.ts
@@ -1,51 +1,55 @@
 export const DOMEN = "http://127.0.0.1:8000/";
 
-
-// import engine from "./assets/engine.png"
-export const OptionsMock = [
+import R1 from "/src/assets/mocking/ГЗ-Э Маршрут.png";
+import R2 from "/src/assets/mocking/МТ-ГЗ Маршрут.png";
+import R3 from "/src/assets/mocking/Э-СМ Маршрут.png";
+import R4 from "/src/assets/mocking/СМ-УЛК Маршрут.png";
+import R5 from "/src/assets/mocking/УЛК-СК Маршрут.png";
+import mgtu from "/src/assets/mocking/2.png";
+export const RouteMock = [
   {
     id: 1,
-    name: "ГЗ",
-    transition_time: 1,
-    description: " Э - ГЗ",
+    name: "МТ - ГЗ",
+    transition_time: 6,
+    description: "Маршрут межда кампусами МТ - ГЗ",
     status: true,
-    buildings:"",
-    transition: 'lol',
+    buildings: mgtu,
+    transition : R1,
   },
   {
     id: 2,
-    name: "Э",
-    transition_time: 10,
-    description: " Э - ГЗ",
+    name: "ГЗ - Э",
+    transition_time: 8,
+    description: "Маршрут межда кампусами ГЗ - Э",
     status: true,
-    buildings:"",
-    transition: 'lol',
+    buildings: mgtu,
+    transition : R2,
   },
   {
     id: 3,
-    name: "СМ",
-    transition_time: 15,
-    description: " Э - ГЗ",
+    name: "Э - СМ",
+    transition_time: 4,
+    description: "Маршрут межда кампусами Э - СМ",
     status: true,
-    buildings:"",
-    transition: 'lol',
+    buildings: mgtu,
+    transition : R3,
   },
   {
     id: 4,
-    name: "УЛК",
-    transition_time: 20,
-    description: " Э - ГЗ",
+    name: "СМ - УЛК",
+    transition_time: 4,
+    description: "Маршрут межда кампусами СМ - УЛК",
     status: true,
-    buildings:"",
-    transition: 'lol',
+    buildings: mgtu,
+    transition : R4,
   },
   {
     id: 5,
-    name: "",
-    transition_time: 10,
-    description: " Э - ГЗ",
+    name: "УЛК - СК",
+    transition_time: 13,
+    description: "Маршрут межда кампусами УЛК - СК",
     status: true,
-    buildings:"",
-    transition: 'lol',
+    buildings: mgtu,
+    transition : R5,
   },
 ];
